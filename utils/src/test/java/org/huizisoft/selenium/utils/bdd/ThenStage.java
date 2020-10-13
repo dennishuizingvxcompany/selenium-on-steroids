02407ab699d603f0a480926ae620596526f9982b
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-10-13 20:47:41 +0200

    removed unused logging object

--- utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
@@ -2,14 +2,11 @@ package org.huizisoft.selenium.utils.bdd;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.huizisoft.selenium.utils.SeleniumContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 public class ThenStage extends Stage<ThenStage> {
-    private static final Logger LOGGER = LogManager.getLogger(ThenStage.class);
     @ExpectedScenarioState
     SeleniumContext seleniumContext;
 
