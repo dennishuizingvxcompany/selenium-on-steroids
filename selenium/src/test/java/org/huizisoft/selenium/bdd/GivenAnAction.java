61208cc76a9d97ec9ded6808587a8ac19c55e254
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-10-13 21:00:48 +0200

    Pulled up context

--- selenium/src/test/java/org/huizisoft/selenium/bdd/GivenAnAction.java
+++ selenium/src/test/java/org/huizisoft/selenium/bdd/GivenAnAction.java
@@ -1,6 +1,7 @@
 package org.huizisoft.selenium.bdd;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.huizisoft.selenium.utils.SeleniumContext;
@@ -8,6 +9,8 @@ import org.huizisoft.selenium.utils.SeleniumContext;
 public class GivenAnAction extends Stage<GivenAnAction> {
 
     private static final Logger LOGGER = LogManager.getLogger(GivenAnAction.class);
+    @ExpectedScenarioState
+    private SeleniumContext seleniumContext;
 
     public GivenAnAction doSomething() {
         LOGGER.info("doSomething works");
@@ -15,9 +18,8 @@ public class GivenAnAction extends Stage<GivenAnAction> {
     }
 
     public GivenAnAction navigateToUrl(String url) {
-        SeleniumContext context = SeleniumContext.getCurrentInstance(true);
-        context.getWebDriver().get(url);
-        LOGGER.info(context.getWebDriver().getPageSource());
+        seleniumContext.getWebDriver().get(url);
+        LOGGER.info(seleniumContext.getWebDriver().getPageSource());
         return self();
     }
 }
--- selenium/src/test/java/org/huizisoft/selenium/tests/MainPageTest.java
+++ selenium/src/test/java/org/huizisoft/selenium/tests/MainPageTest.java
@@ -1,15 +1,21 @@
 package org.huizisoft.selenium.tests;
 
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.junit5.ScenarioTest;
 import org.huizisoft.selenium.bdd.GivenAnAction;
 import org.huizisoft.selenium.bdd.ThenSomethingIsExpected;
 import org.huizisoft.selenium.bdd.WhenSomethingOccurs;
 import org.huizisoft.selenium.utils.SeleniumContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainPageTest extends ScenarioTest<GivenAnAction, WhenSomethingOccurs, ThenSomethingIsExpected> {
+
+    @ProvidedScenarioState
+    private final SeleniumContext seleniumContext = SeleniumContext.getCurrentInstance(true);
+
     @Test
     void testMainPage() {
         given().navigateToUrl("http://tomcat:8080/testapp/index.jsp");
@@ -23,5 +29,11 @@ class MainPageTest extends ScenarioTest<GivenAnAction, WhenSomethingOccurs, Then
         SeleniumContext driver = SeleniumContext.getCurrentInstance(true);
         driver.getWebDriver().get("http://tomcat:8080/testapp/index.jsp");
         assertTrue(driver.getWebDriver().getCurrentUrl().endsWith("index.jsp"));
+        driver.closeWebDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        seleniumContext.closeWebDriver();
     }
 }
