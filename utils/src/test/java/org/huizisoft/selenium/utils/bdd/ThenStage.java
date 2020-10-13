ce38da08ac45b9092583a4f14f37577a4dd24ab5
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-10-13 19:55:55 +0200

    renamed method

--- utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
@@ -2,15 +2,18 @@ package org.huizisoft.selenium.utils.bdd;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.huizisoft.selenium.utils.SeleniumContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 public class ThenStage extends Stage<ThenStage> {
+    private static final Logger LOGGER = LogManager.getLogger(ThenStage.class);
     @ExpectedScenarioState
     SeleniumContext seleniumContext;
 
-    public ThenStage the_selenium_context_is_not_null(Object expectedObject) {
+    public ThenStage the_selenium_context_is_not_$(Object expectedObject) {
         assertNotSame(seleniumContext, expectedObject);
         return self();
     }
