074f76ccdb6c6ce804560f2d3f0a82737b785627
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-12-23 22:13:29 +0100

    Changed logging

--- testapp/src/main/java/org/huizisoft/frontend/controllers/Controller.java
+++ testapp/src/main/java/org/huizisoft/frontend/controllers/Controller.java
@@ -1,16 +1,17 @@
 package org.huizisoft.frontend.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.huizisoft.frontend.utils.FileLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStream;
 
+
 @RestController
 public class Controller {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+    private static final Logger LOGGER = LogManager.getLogger(Controller.class);
 
     @RequestMapping("/")
     public String index() {
