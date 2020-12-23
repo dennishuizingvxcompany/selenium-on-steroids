efeb6975af34b93bc5156c053d8ea8ea97501b4c
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-12-23 21:29:36 +0100

    Added needed configuration and added a log line close #5

--- testapp/src/main/java/org/huizisoft/frontend/controllers/Controller.java
+++ testapp/src/main/java/org/huizisoft/frontend/controllers/Controller.java
@@ -1,6 +1,8 @@
 package org.huizisoft.frontend.controllers;
 
 import org.huizisoft.frontend.utils.FileLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,8 @@ import java.io.InputStream;
 
 @RestController
 public class Controller {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
     @RequestMapping("/")
     public String index() {
         return loadStaticPage().toString();
@@ -16,6 +20,7 @@ public class Controller {
     private StringBuilder loadStaticPage() {
         FileLoader fileLoader = new FileLoader();
         InputStream input = fileLoader.getFileFromResourceAsStream("index.jsp");
+        LOGGER.info("File input stream is returned");
         return fileLoader.printInputStream(input);
     }
 }
