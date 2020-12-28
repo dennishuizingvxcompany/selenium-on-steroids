6061a2902e9df3614f52bbf0e03f8b8401b00b3d
dennishuizingvxcompany <dhuizing@vxcompany.com>
2020-12-28 16:30:31 +0100

    Adding class closes 6

---
+++ utils/src/main/java/org/huizisoft/selenium/utils/SeleniumContextHelper.java
@@ -0,0 +1,13 @@
+package org.huizisoft.selenium.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SeleniumContextHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeleniumContextHelper.class);
+
+    public SeleniumContextHelper() {
+        //TODO: Implement something useful
+        LOGGER.info("Implement something useful");
+    }
+}
