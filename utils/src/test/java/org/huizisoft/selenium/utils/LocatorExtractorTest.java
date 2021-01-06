4770dc5347894399d3f17c8593d24caa18e19eb9
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-06 20:43:27 +0100

    - renamed field and method - Cleaned up unused loggers - refactored JGiven variables

--- utils/src/test/java/org/huizisoft/selenium/utils/LocatorExtractorTest.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/LocatorExtractorTest.java
@@ -20,7 +20,7 @@ class LocatorExtractorTest extends ScenarioTest<LocatorExtractorTest.Given
         , LocatorExtractorTest.Then> {
 
     @FindBy(css = "button8")
-    private WebElement blaat;
+    private WebElement proxyWebElement;
 
     public LocatorExtractorTest() {
         PageFactory.initElements(SeleniumContext.getDefaultWebDriver(), this);
@@ -46,14 +46,13 @@ class LocatorExtractorTest extends ScenarioTest<LocatorExtractorTest.Given
     @Test
     void testExtractorWith() {
         given().we_have_a_selenium_context()
-                .and().a_web_element_that_dont_exist(blaat);
+                .and().a_proxy_web_element(proxyWebElement);
         when().we_extract_the_locator();
         then().we_should_get_a_by();
     }
 
     static class Given extends Stage<Given> {
         private static final String DEFAULT_URL = "http://tomcat:8080/testapp/";
-        private static final Logger LOGGER = LogManager.getLogger(Given.class);
 
         @ProvidedScenarioState
         private WebElement element;
@@ -70,17 +69,16 @@ class LocatorExtractorTest extends ScenarioTest<LocatorExtractorTest.Given
             return self();
         }
 
-        Given a_web_element_that_dont_exist(WebElement webElement) {
+        Given a_proxy_web_element(WebElement webElement) {
             element = webElement;
             return self();
         }
     }
 
     static class When extends Stage<When> {
-        private static final Logger LOGGER = LogManager.getLogger(When.class);
         @ExpectedScenarioState
         private WebElement element;
-        @ExpectedScenarioState
+        @ProvidedScenarioState
         private By byOfElement;
 
         When we_extract_the_locator() {
