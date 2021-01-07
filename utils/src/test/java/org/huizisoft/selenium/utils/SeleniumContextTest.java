44134ff325e266e5a16c76d4c73bef282a3f78d7
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-07 21:29:23 +0100

    #19 Fixed tests and added one

--- utils/src/test/java/org/huizisoft/selenium/utils/SeleniumContextTest.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/SeleniumContextTest.java
@@ -1,6 +1,5 @@
 package org.huizisoft.selenium.utils;
 
-import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.junit5.ScenarioTest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,54 +10,57 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class SeleniumContextTest extends ScenarioTest<SeleniumGivenStage, WhenStage, ThenStage> {
-
     private static final Logger LOGGER = LogManager.getLogger(SeleniumContextTest.class);
-
-    @ProvidedScenarioState
-    private final SeleniumContext seleniumContext = SeleniumContext.createInstance();
 
     @Test
     @SuppressWarnings("java:S2699")
     void verifyCreateInstanceTest() {
-        given().the_selenium_context_is_created(seleniumContext);
-        then().the_selenium_context_is_not_$(null);
+        given().the_selenium_context_is_created();
+        then().the_selenium_context_is_empty$(false);
     }
 
     @Test
     @SuppressWarnings("java:S2699")
     void getCurrentInstance() {
         when().the_current_instance_is_created_with_new_object_or_not(false);
-        then().the_selenium_context_is_not_$(seleniumContext);
+        then().the_selenium_context_is_empty$(true);
+    }
+
+    @Test
+    @SuppressWarnings("java:S2699")
+    void getCurrentInstanceWithNewObject() {
+        when().the_current_instance_is_created_with_new_object_or_not(true);
+        then().the_selenium_context_is_empty$(false);
     }
 
     @Test
     @SuppressWarnings("java:S2699")
     void setCurrentInstance() {
-        given().the_selenium_context_is_created(seleniumContext);
+        given().the_selenium_context_is_created();
         when().the_current_instance_is_set_to_current_selenium_context();
-        then().the_selenium_context_is_not_$(null);
+        then().the_selenium_context_is_empty$(false);
     }
 
     @Test
     @SuppressWarnings("java:S2699")
     void closeWebDriver() {
-        given().the_selenium_context_is_created(seleniumContext);
+        given().the_selenium_context_is_created();
         when().the_web_driver_is_closed();
-        then().the_selenium_context_is_not_$(null)
+        then().the_selenium_context_is_empty$(true)
                 .and().the_web_driver_is_running(false);
     }
 
     @Test
     @SuppressWarnings("java:S2699")
     void isWebDriverRunning() {
-        given().the_selenium_context_is_created(seleniumContext);
+        given().the_selenium_context_is_created();
         then().the_web_driver_is_running(true);
     }
 
     @Test
     @SuppressWarnings("java:S2699")
     void setSeleniumServerBaseUrlAndGetTheNewBaseUrl() {
-        given().the_selenium_context_is_created(seleniumContext);
+        given().the_selenium_context_is_created();
         when().the_selenium_server_base_url_is_set_to("http://fake.base.url");
         then().the_base_url_is_$("http://fake.base.url");
     }
