a89a9d320ccc6c1602463320132eb1975e1c2f02
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-15 13:32:30 +0100

    #19 Removed unused implements

--- utils/src/test/java/org/huizisoft/selenium/utils/SeleniumContextTestConditionerExtension.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/SeleniumContextTestConditionerExtension.java
@@ -1,32 +1,14 @@
 package org.huizisoft.selenium.utils;
 
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SeleniumContextTestConditionerExtension implements
-        BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+        BeforeEachCallback {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SeleniumContextTestConditionerExtension.class);
-
-    @Override
-    public void afterAll(ExtensionContext extensionContext) {
-
-    }
-
-    @Override
-    public void afterEach(ExtensionContext extensionContext) {
-        SeleniumContext.closeWebDriver();
-    }
-
-    @Override
-    public void beforeAll(ExtensionContext extensionContext) {
-
-    }
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) {
