3f7c1681fa055c67ccb8b859b94df9f9c97f07aa
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-15 12:25:38 +0100

    #19 Using the extension now and log4j

--- utils/src/test/java/org/huizisoft/selenium/utils/LocatorExtractorTest.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/LocatorExtractorTest.java
@@ -3,12 +3,11 @@ package org.huizisoft.selenium.utils;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.junit5.ScenarioTest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.huizisoft.selenium.utils.bdd.SeleniumGivenStage;
 import org.huizisoft.selenium.utils.bdd.ThenStage;
 import org.huizisoft.selenium.utils.bdd.WhenStage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,12 +15,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(value = {SeleniumContextTestConditionerExtension.class})
 class LocatorExtractorTest extends ScenarioTest<LocatorExtractorTest.Given
         , LocatorExtractorTest.When
         , LocatorExtractorTest.Then> {
@@ -107,7 +109,7 @@ class LocatorExtractorTest extends ScenarioTest<LocatorExtractorTest.Given
     }
 
     static class Then extends ThenStage<Then> {
-        private static final Logger LOGGER = LogManager.getLogger(Then.class);
+        private static final Logger LOGGER = LoggerFactory.getLogger(Then.class);
         @ExpectedScenarioState
         private By byOfElement;
 
