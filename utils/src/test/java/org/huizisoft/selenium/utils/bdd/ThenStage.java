520cfe9d1ee73b0934ee7a962ffd60b8b289ee66
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-15 10:40:23 +0100

    #19 Removed logger

--- utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/bdd/ThenStage.java
@@ -2,8 +2,6 @@ package org.huizisoft.selenium.utils.bdd;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.huizisoft.selenium.utils.SeleniumBaseUrl;
 import org.huizisoft.selenium.utils.SeleniumContext;
 
@@ -13,17 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ThenStage<SELF extends Stage<?>> extends Stage<SELF> {
-    private static final Logger LOGGER = LogManager.getLogger(ThenStage.class);
     @ExpectedScenarioState
     SeleniumContext seleniumContext;
-
-    static void tearDown() {
-        int counter = 0;
-        while (SeleniumContext.getDefaultWebDriver() != null) {
-            SeleniumContext.closeWebDriver();
-            LOGGER.info("Number of time i tried to close the WebDriver {}", counter);
-        }
-    }
 
     public SELF the_selenium_context_is_empty$(boolean emptyOrNot) {
         if (emptyOrNot) {
