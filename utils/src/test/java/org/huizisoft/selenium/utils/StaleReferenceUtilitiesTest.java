43962a596b24738cb7bc3cbf639ada7f96c9af0f
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-15 13:11:23 +0100

    #19 Adding test cases

--- utils/src/test/java/org/huizisoft/selenium/utils/StaleReferenceUtilitiesTest.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/StaleReferenceUtilitiesTest.java
@@ -1,5 +1,6 @@
 package org.huizisoft.selenium.utils;
 
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.junit5.ScenarioTest;
 import org.huizisoft.selenium.utils.bdd.SeleniumGivenStage;
@@ -8,8 +9,11 @@ import org.huizisoft.selenium.utils.bdd.WhenStage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SeleniumContextTestConditionerExtension.class)
@@ -18,7 +22,26 @@ class StaleReferenceUtilitiesTest extends ScenarioTest<StaleReferenceUtilitiesTe
         , StaleReferenceUtilitiesTest.Then> {
 
     @Test
-    void testGetPossibleExceptionOfElement() {
+    void testGetPossibleExceptionOfElementWithANonStaleElement() {
+        given().the_selenium_context_is_created()
+                .and().navigate_to_default_testapp()
+                .and().a_web_element(By.id("button1"));
+        when().the_element_is_check_on_a_possible_stale_reference_exception();
+        then().an_exception_is_expected(new NullPointerException(), "No exception is thrown");
+    }
+
+    @Test
+    void testGetPossibleExceptionOfElementWithAStaleElement() {
+        given().the_selenium_context_is_created()
+                .and().navigate_to_default_testapp()
+                .and().a_web_element(By.id("button1"));
+        when().the_page_is_refreshed()
+                .and().the_element_is_check_on_a_possible_stale_reference_exception();
+        then().an_exception_is_expected(new StaleElementReferenceException(""), "element is not attached to the page document");
+    }
+
+    @Test
+    void testCheckIfElementHasStaleReference() {
         given().the_selenium_context_is_created()
                 .and().navigate_to_default_testapp()
                 .and().a_web_element(By.id("button1"));
@@ -28,13 +51,14 @@ class StaleReferenceUtilitiesTest extends ScenarioTest<StaleReferenceUtilitiesTe
     }
 
     @Test
-    void testCheckIfElementHasStaleReference() {
-        //TODO: Implement test
-    }
-
-    @Test
     void testReturnNonStaleWebElement() {
-        //TODO: Implement test
+        given().the_selenium_context_is_created()
+                .and().navigate_to_default_testapp()
+                .and().a_web_element(By.id("button1"));
+
+        when().the_page_is_refreshed()
+                .and().a_non_stale_element_needs_to_be_returned();
+        then().a_non_stale_referenced_web_element_is_returned();
     }
 
     @Test
@@ -54,8 +78,20 @@ class StaleReferenceUtilitiesTest extends ScenarioTest<StaleReferenceUtilitiesTe
     public static class When extends WhenStage<When> {
         @ProvidedScenarioState
         private WebElement webElement;
+        @ProvidedScenarioState
+        private Exception exception;
 
         public WhenStage<When> the_stale_state_of_a_web_element_is_checked() {
+            StaleReferenceUtilities.checkIfElementHasAStaleReference(webElement);
+            return self();
+        }
+
+        When the_element_is_check_on_a_possible_stale_reference_exception() {
+            exception = StaleReferenceUtilities.getPossibleExceptionOfElement(webElement);
+            return self();
+        }
+
+        When a_non_stale_element_needs_to_be_returned() {
             StaleReferenceUtilities.returnNonStaleWebElement(webElement);
             return self();
         }
@@ -64,9 +100,22 @@ class StaleReferenceUtilitiesTest extends ScenarioTest<StaleReferenceUtilitiesTe
     public static class Then extends ThenStage<Then> {
         @ProvidedScenarioState
         private WebElement webElement;
+        @ExpectedScenarioState
+        private Exception exception;
 
         public Then a_non_stale_referenced_web_element_is_returned() {
             assertTrue(StaleReferenceUtilities.checkIfElementHasAStaleReference(webElement));
+            return self();
+        }
+
+        public Then an_exception_is_expected(Exception e, String containsText) {
+            assertEquals(e.getClass(), exception.getClass());
+            assertTrue(exception.getMessage().contains(containsText));
+            return self();
+        }
+
+        public Then no_exception_is_expected() {
+            assertNull(exception);
             return self();
         }
     }
