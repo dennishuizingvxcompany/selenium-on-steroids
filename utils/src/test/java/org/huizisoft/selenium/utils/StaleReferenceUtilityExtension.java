c9b04c9c5319386a42943e421519c1cc5471579b
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-01-15 16:21:32 +0100

    #19 After seems better?!

--- utils/src/test/java/org/huizisoft/selenium/utils/StaleReferenceUtilityExtension.java
+++ utils/src/test/java/org/huizisoft/selenium/utils/StaleReferenceUtilityExtension.java
@@ -2,14 +2,14 @@ package org.huizisoft.selenium.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class StaleReferenceUtilityExtension implements BeforeEachCallback {
+public class StaleReferenceUtilityExtension implements AfterEachCallback {
     private static final Logger LOGGER = LogManager.getLogger(StaleReferenceUtilityExtension.class);
 
     @Override
-    public void beforeEach(ExtensionContext extensionContext) {
+    public void afterEach(ExtensionContext extensionContext) {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("Setting default max number of refresh attempts");
         }
