9367c25c1b41ab0bdee2802ebd6fb8496b8bf1d3
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-03-06 10:41:13 +0100

    #21 tried to add different webdriver waits but that does not seem to work!

--- utils/src/test/java/org/huizisoft/selenium/bdd/GivenStage.java
+++ utils/src/test/java/org/huizisoft/selenium/bdd/GivenStage.java
@@ -4,12 +4,15 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import org.huizisoft.selenium.SeleniumContext;
 import org.huizisoft.selenium.configuration.SeleniumBaseUrl;
+import org.huizisoft.selenium.pom.TestPage;
 import org.huizisoft.selenium.utils.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class GivenStage extends Stage<GivenStage> {
     private static final String DEFAULT_URL = "http://tomcat:8080/testapp/";
@@ -20,6 +23,8 @@ public class GivenStage extends Stage<GivenStage> {
 
     @FindBy(css = "button8")
     private WebElement proxyWebElement;
+
+    private TestPage testPage;
 
     public GivenStage the_selenium_context_is_created() {
         try {
@@ -61,4 +66,19 @@ public class GivenStage extends Stage<GivenStage> {
         return self();
     }
 
+    public GivenStage the_button_is_set_to_hidden() {
+        testPage.toggleVisibilityOfHiddenButton();
+        return self();
+    }
+
+    public GivenStage the_test_page_object_is_being_initialized() {
+        testPage = new TestPage();
+        return self();
+    }
+
+    public GivenStage the_selenium_context_time_out_is_set_to() {
+        SeleniumContext.getRemoteWebDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        SeleniumContext.getRemoteWebDriver().manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        return self();
+    }
 }
