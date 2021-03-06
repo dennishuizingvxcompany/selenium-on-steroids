e284db2448523533587f0e2a4505fef5ee0a0031
dennishuizingvxcompany <dhuizing@vxcompany.com>
2021-03-06 10:30:26 +0100

    #21 Adding new element

--- utils/src/test/java/org/huizisoft/selenium/pom/TestPage.java
+++ utils/src/test/java/org/huizisoft/selenium/pom/TestPage.java
@@ -1,19 +1,24 @@
 package org.huizisoft.selenium.pom;
 
+import org.huizisoft.selenium.SeleniumContext;
 import org.huizisoft.selenium.utils.BasePage;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TestPage extends BasePage {
-    @FindBy
     private WebElement toggleButton;
+    private WebElement hiddenButton;
+
+    public TestPage() {
+        PageFactory.initElements(SeleniumContext.getRemoteWebDriver(), this);
+    }
 
     public void toggleVisibilityOfHiddenButton() {
         waitForElementPresentAndDisplayed(toggleButton);
         clickElement(toggleButton);
     }
 
-    public WebElement getToggleButton() {
-        return toggleButton;
+    public WebElement getHiddenButton() {
+        return hiddenButton;
     }
 }
